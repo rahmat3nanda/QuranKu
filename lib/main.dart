1206e0ab373852ec4771d6f6c9ff906f766b7b2e
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-24 23:52:21 +0700

    config

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,32 @@
  *
  */
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:quranku/app.dart';
+import 'package:quranku/common/configs.dart';
+import 'package:quranku/common/constants.dart';
+import 'package:quranku/model/app/app_version_model.dart';
+import 'package:quranku/model/app/scheme_model.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FlutterError.onError = (errorDetails) {
+    AppLog.print(errorDetails);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    AppLog.print(error);
+    return true;
+  };
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  AppLog.debugMode = false;
+  AppConfig.shared.initialize(
+    scheme: AppScheme.prod,
+    baseUrlApi: "https://equran.id/api/v2/",
+    version: AppVersionModel.empty(),
+  );
   runApp(const App());
 }
