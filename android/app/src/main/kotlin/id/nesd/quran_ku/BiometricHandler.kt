a12216ccb534ab01ea54469907166d5d181a9686
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-30 17:07:32 +0700

    create BiometricHandler

---
+++ android/app/src/main/kotlin/id/nesd/quran_ku/BiometricHandler.kt
@@ -0,0 +1,55 @@
+package id.nesd.quran_ku
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import java.util.concurrent.Executors
+
+internal object BiometricHandler {
+    fun isBiometricAvailable(context: Context): Boolean {
+        val biometricManager = BiometricManager.from(context)
+        return when (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK)) {
+            BiometricManager.BIOMETRIC_SUCCESS -> true
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE,
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE,
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> false
+
+            else -> false
+        }
+    }
+
+    fun showBiometricPrompt(
+        context: Context,
+        callback: (success: Boolean, message: String) -> Unit
+    ) {
+        val executor = Executors.newSingleThreadExecutor()
+        val biometricPrompt = BiometricPrompt(
+            context as AppCompatActivity,
+            executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    callback(false, errString.toString())
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    callback(true, "Auth Success")
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    callback(false, "Auth Failure")
+                }
+            })
+
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric Login")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Use account password")
+            .build()
+
+        biometricPrompt.authenticate(promptInfo)
+    }
+}
