8ab4eae986f4c6652ccd3e8c0b766bc7a1f22a80
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-30 17:39:05 +0700

    change extends

--- android/app/src/main/kotlin/id/nesd/quran_ku/MainActivity.kt
+++ android/app/src/main/kotlin/id/nesd/quran_ku/MainActivity.kt
@@ -1,11 +1,11 @@
 package id.nesd.quranku
 
 import id.nesd.quran_ku.BiometricHandler
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
     private val channel = "id.nesd.quranku.channel"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
@@ -16,7 +16,7 @@ class MainActivity : FlutterActivity() {
             channel
         ).setMethodCallHandler { call, result ->
             if (call.method == "checkFingerprint") {
-                if (!BiometricHandler.isBiometricAvailable(context)) {
+                if (!BiometricHandler.isBiometricAvailable(this)) {
                     result.error("1", "Biometric Not Available", null)
                     return@setMethodCallHandler
                 }
@@ -25,12 +25,12 @@ class MainActivity : FlutterActivity() {
                 return@setMethodCallHandler
             }
             if (call.method == "authFingerprint") {
-                if (!BiometricHandler.isBiometricAvailable(context)) {
+                if (!BiometricHandler.isBiometricAvailable(this)) {
                     result.error("2", "Biometric Not Available", null)
                     return@setMethodCallHandler
                 }
 
-                BiometricHandler.showBiometricPrompt(context) { success, message ->
+                BiometricHandler.showBiometricPrompt(this) { success, message ->
                     if (success) {
                         result.success(true)
                     } else {
